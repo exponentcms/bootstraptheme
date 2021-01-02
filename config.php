ff22710851760e1d52b9d290df90662b32353d4a
dleffler <dleffler@hughes.net>
2021-01-02 09:28:03 -0500

    Initial commit for repository. Theme no longer shipped with main package

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define("BTN_COLOR",'btn');
+define("SWATCH",'united');
+define("BTN_SIZE",'medium');
+define("MENU_LOCATION",'fixed-top');
+define("MENU_HEIGHT",'1');
+define("MENU_WIDTH",'769');
+define("MENU_ALIGN",'left');
+define("MENU_LENGTH",'2');
+?>
